4583fd7b28d06f7d96456fce14c7a5d99a8a95aa
Danilo Itagyba <daniloitagyba@gmail.com>
2026-05-11 02:02:23 -0300

    Revert "Try early profile fast path"
    This reverts commit 7b4d4148dc2e243915fe89ab3864b769edeb8678.

--- src/RinhaFraud/BinaryIndex.cs
+++ src/RinhaFraud/BinaryIndex.cs
@@ -1002,7 +1002,7 @@ CandidateSearchDone:
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public bool TryProfileFastDecision(ReadOnlySpan<short> query, in SearchParams searchParams, out int fraudCount)
+    private bool TryProfileFastDecision(ReadOnlySpan<short> query, in SearchParams searchParams, out int fraudCount)
     {
         fraudCount = 0;
         if (!searchParams.ProfileFastPath)
--- src/RinhaFraud/HttpServer.cs
+++ src/RinhaFraud/HttpServer.cs
@@ -299,17 +299,6 @@ internal static class HttpServer
         {
             var bodyStart = headerEnd + 4;
             var body = request.Slice(bodyStart, contentLength);
-            if (searchParams.ProfileFastPath)
-            {
-                Span<short> profileQuery = stackalloc short[Constants.Dim];
-                if (QueryBuilder.TryBuildProfileQuery(body, profileQuery) &&
-                    index.TryProfileFastDecision(profileQuery, searchParams, out var profileFraudCount))
-                {
-                    SendDecision(socket, profileFraudCount);
-                    return;
-                }
-            }
-
             Span<short> query = stackalloc short[Constants.Dim];
             if (!QueryBuilder.TryBuildQuery(body, query))
             {
@@ -446,24 +435,6 @@ internal static class HttpServer
         {
             var bodyStart = headerEnd + 4;
             var body = request.Slice(bodyStart, contentLength);
-            if (searchParams.ProfileFastPath)
-            {
-                Span<short> profileQuery = stackalloc short[Constants.Dim];
-                if (QueryBuilder.TryBuildProfileQuery(body, profileQuery) &&
-                    index.TryProfileFastDecision(profileQuery, searchParams, out var profileFraudCount))
-                {
-                    return profileFraudCount switch
-                    {
-                        <= 0 => Approved00ResponseBytes,
-                        1 => Approved02ResponseBytes,
-                        2 => Approved04ResponseBytes,
-                        3 => Denied06ResponseBytes,
-                        4 => Denied08ResponseBytes,
-                        _ => Denied10ResponseBytes
-                    };
-                }
-            }
-
             Span<short> query = stackalloc short[Constants.Dim];
             if (!QueryBuilder.TryBuildQuery(body, query))
             {
